3075f6d81b93c12ac4bc5f94071cf274339140a1
Diego Vilela <vilelaphp@gmail.com>
2021-07-01 18:30:00 -0300

    Add page to list episodes

--- vocab/urls.py
+++ vocab/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug:slug>/', views.entry_detail, name='entry_detail'),
     path('episode/<slug:slug>/', views.episode_detail, name='episode_detail'),
+    path('episodes/', views.episode_list, name='episode_list'),
+    path('<slug:slug>/', views.entry_detail, name='entry_detail'),
 ]
--- vocab/views.py
+++ vocab/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.template.response import TemplateResponse
 
@@ -17,4 +18,14 @@ def entry_detail(request, slug):
 def episode_detail(request, slug):
     return TemplateResponse(request, 'vocab/episode_detail.html', {
         'episode': get_object_or_404(Episode.objects.all(), slug=slug)
+    })
+
+
+def episode_list(request):
+    episodes = Episode.objects.all()
+    paginator = Paginator(episodes, 100)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'vocab/episode_list.html', {
+        'page_obj': page_obj,
     })
