9b81b0c8b5954031967c25489001bd179b5311ae
Diego Vilela <vilelaphp@gmail.com>
2021-07-20 21:08:06 -0300

    Improve bbc module

--- vocab/bbc.py
+++ vocab/bbc.py
@@ -13,6 +13,98 @@ URL_BASE = 'https://www.bbc.co.uk/learningenglish/english/course/newsreview/unit
 UnitInfo = namedtuple('UnitInfo', ['unit', 'starting_episode', 'ending_episode'])
 
 
+def _remove_links(bs: BeautifulSoup):
+    for el in bs(('p', 'h3')):
+        if el.find('a', href=True):
+            el.decompose()
+
+
+def _get_unit_from_episode_id(ep_id):
+    if ep_id <= 0:
+        raise Exception('Invalid episode_id.')
+    elif 1 <= ep_id <= 109:
+        unit = int(ep_id / 10 + 1)
+    elif 110 <= ep_id <= 139:
+        unit = int(ep_id / 10)
+    else:  # >= 140
+        unit = int(ep_id / 10 - 1)
+    return unit
+
+
+def _get_unit_info(unit=None, episode_id=None):
+    """Return a UnitInfo object as per the BBC site."""
+
+    if episode_id:
+        unit = _get_unit_from_episode_id(episode_id)
+
+    start = (unit * 10) - 9  # 71
+    end = start + 9  # 80
+    if unit == 10:
+        end = 99
+    elif unit == 11:
+        start = 100
+        end = 119
+    elif unit == 12:
+        start = unit * 10
+        end = start + 9
+    elif unit == 13:
+        start = unit * 10
+        end = 149
+    elif unit >= 14:
+        start = (unit * 10) + 10
+        end = start + 9
+
+    return UnitInfo(unit, start, end)
+
+
+def _isRelevant(text: str):
+    if text.startswith('_'):
+        return False
+
+    to_exclude = {
+        'Downloads',
+        'More',
+        'Did you like that? Why not try these?',
+        'Watch the video and complete the activity',
+        'To do',
+        "Try our quiz to see how well you've learned today's language.",
+    }
+
+    if text in to_exclude:
+        return False
+
+    return True
+
+
+def _get_episode_content(sections):
+    content = []
+    for section in sections:
+        for line in section.stripped_strings:
+            line_text = str(line)
+            if _isRelevant(line_text):
+                content.append(line)
+
+    return content[1:]
+
+
+def _parse_episode(content, episode_id: int):
+    bs = BeautifulSoup(content, features="html5lib")
+    _remove_links(bs)
+    sections = bs.find_all('div', class_=re.compile('widget widget-richtext'))
+    headline = sections[0].find('h3').string
+    headline = str(headline)
+    ep_content = _get_episode_content(sections)
+    ep_content = '\n'.join(ep_content)
+    ep_content = ep_content.replace(u'\xa0', u' ')
+    episode = Episode(
+        id=episode_id,
+        headline=headline,
+        slug=slugify(headline),
+        raw_content=ep_content,
+    )
+    return episode
+
+
 class NewsReview:
     def __init__(self):
         self._session = requests.Session()
@@ -34,92 +126,6 @@ class NewsReview:
             except IndexError as e:
                 print(f'Error: {e}')
 
-    def _get_unit_from_episode_id(self, ep_id):
-        if ep_id <= 0:
-            raise Exception('Invalid episode_id.')
-        elif 1 <= ep_id <= 109:
-            unit = int(ep_id / 10 + 1)
-        elif 110 <= ep_id <= 139:
-            unit = int(ep_id / 10)
-        else:  # >= 140
-            unit = int(ep_id / 10 - 1)
-        return unit
-
-    def _get_unit_info(self, unit=None, episode_id=None):
-        """Return a UnitInfo object as per the BBC site."""
-
-        if episode_id:
-            unit = self._get_unit_from_episode_id(episode_id)
-
-        start = (unit * 10) - 9  # 71
-        end = start + 9  # 80
-        if unit == 10:
-            end = 99
-        elif unit == 11:
-            start = 100
-            end = 119
-        elif unit == 12:
-            start = unit * 10
-            end = start + 9
-        elif unit == 13:
-            start = unit * 10
-            end = 149
-        elif unit >= 14:
-            start = (unit * 10) + 10
-            end = start + 9
-
-        return UnitInfo(unit, start, end)
-
-    def _remove_links(self, bs: BeautifulSoup):
-        for el in bs(('p', 'h3')):
-            if el.find('a', href=True):
-                el.decompose()
-
-    def _isRelevant(self, text: str):
-        if text.startswith('_'):
-            return False
-
-        to_exclude = {
-            'Downloads',
-            'More',
-            'Did you like that? Why not try these?',
-            'Watch the video and complete the activity',
-            'To do',
-            "Try our quiz to see how well you've learned today's language.",
-        }
-
-        if text in to_exclude:
-            return False
-
-        return True
-
-    def _get_episode_content(self, sections):
-        content = []
-        for section in sections:
-            for line in section.stripped_strings:
-                line_text = str(line)
-                if self._isRelevant(line_text):
-                    content.append(line)
-
-        return content[1:]
-
-    def _parse_episode(self, content, episode_id: int):
-        bs = BeautifulSoup(content, features="html5lib")
-        self._remove_links(bs)
-        sections = bs.find_all('div', class_=re.compile('widget widget-richtext'))
-        headline = sections[0].find('h3').string
-        headline = str(headline)
-        ep_content = self._get_episode_content(sections)
-        ep_content = '\n'.join(ep_content)
-        ep_content = ep_content.replace(u'\xa0', u' ')
-        episode = Episode(
-            id=episode_id,
-            headline=headline,
-            slug=slugify(headline),
-            raw_content=ep_content,
-        )
-        return episode
-
     def _get_episode(self, episode_id, unit):
         # Due to BBC bug
         if episode_id == 1:
@@ -132,7 +138,7 @@ class NewsReview:
             response.raise_for_status()
             if response.status_code == 404:
                 print(f'Episode {episode_id} was not published yet.')
-            return self._parse_episode(response.content, episode_id)
+            return _parse_episode(response.content, episode_id)
         except requests.exceptions.RequestException as e:
             print(f'Error: {e}')
             return False
@@ -141,7 +147,7 @@ class NewsReview:
         # Uses a unit to get episode dates, otherwise, one
         # more request would be necessary for each episode
         # as their pages don't have their dates
-        unit_info = self._get_unit_info(unit)
+        unit_info = _get_unit_info(unit)
         start = from_ep or unit_info.starting_episode
         end = unit_info.ending_episode
 
@@ -185,7 +191,7 @@ class NewsReview:
         # Update. There are Episodes in the database
         if Episode.objects.exists():
             from_episode_id = Episode.objects.last().pk + 1
-            unit = self._get_unit_info(episode_id=from_episode_id).unit
+            unit = _get_unit_info(episode_id=from_episode_id).unit
 
         # Run the fetching and parsing
         result = []
